b762e6fbbc361f2dbaedc82beacaebfb5ff76bc3
PawanKumar Yandapalli <145503387+pawanyandapalli7@users.noreply.github.com>
2026-02-10 20:21:27 -0800

    Create glue_etl_job.py
    Add AWS Glue PySpark ETL sample

---
+++ 04_cloud_aws/glue_lambda_samples/glue_etl_job.py
@@ -0,0 +1,29 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.functions import col, to_date
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+claims_df = spark.read.parquet("s3://claims-raw-data/")
+
+clean_df = (
+    claims_df
+    .filter(col("claim_id").isNotNull())
+    .withColumn("claim_date", to_date(col("claim_date")))
+    .dropDuplicates(["claim_id"])
+)
+
+clean_df.write.mode("overwrite").parquet(
+    "s3://claims-curated-data/"
+)
+
+job.commit()
